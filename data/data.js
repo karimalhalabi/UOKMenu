e7f8e5b536ef0bac0b2b1802af2f9be1d9b26d0b
karimalhalabi <46349003+karimalhalabi@users.noreply.github.com>
2021-01-06 12:32:07 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,12 @@
+        var nodeDataArray = [
+        { key: 0, id: 0, name: "كلية الطب"             , pictureFile: "MED", birthYear: "1865",
+
+         deathYear: "1936", reign: "1910-1936" },
+        { key: 1, id: 1, name: "كلية الهندسة"          , gender: "M", birthYear: "1894", deathYear: "1972", reign: "1936" },
+        { key: 2, id: 2, name: "كلية الصيدلة"          , gender: "M", birthYear: "1895", deathYear: "1952", reign: "1936-1952" },
+        { key: 3, id: 3, name: "كلية الهندسة المعمارية", gender: "M", birthYear: "1897", deathYear: "1965" },
+        { key: 4, id: 4, name: "كلية طب الأسنان"       , gender: "M", birthYear: "1900", deathYear: "1974" },
+        { key: 5, id: 5, name: "كلية العلوم"           , gender: "F", birthYear: "1926", reign: "1952-" },
+        { key: 6, id: 6, name: "كلية إدارة الأعمال"    , gender: "M", birthYear: "1900", deathYear: "1974" },
+        { key: 7, id: 7, name: "كلية لاشئ"             , gender: "F", birthYear: "1926", reign: "1952-" },
+      ];
